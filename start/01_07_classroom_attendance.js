5239c607188becfdb316ca2121747aa096df716a
wasim altayyan <wasimtayyan@gmail.com>
2023-09-03 18:28:00 +0300

    07 done

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,14 @@
 // Write your code here
+function getStudent(classRoom) {
+  let [hasAssistnt, classList] = classRoom
+  let teacher, assistnt, students
+
+  if (hasAssistnt) {
+    [teacher, assistnt, ...students] = classList
+  } else {
+    [teacher, ...students] = classList
+  }
+  return students
+}
+console.log(getStudent([true, ['rew', 'werwe', 'rewrw', 'werwer']]))
+console.log(getStudent([false, ['rew', 'laly', 'somer', 'rats']]))

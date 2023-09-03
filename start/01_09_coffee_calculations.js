1dc313d23ef620fc64192942926ecc0ed3b7270e
wasim altayyan <wasimtayyan@gmail.com>
2023-09-03 19:11:57 +0300

    09 is done

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,8 @@
 // Write your code here
+function Totle(array) {
+
+  let sum = array.reduce((acc, cournt) => (acc += cournt))
+  const bill = sum * 1.25
+  return `the totle bill is ${bill}`
+}
+console.log(Totle([2, 3, 1, 5]))

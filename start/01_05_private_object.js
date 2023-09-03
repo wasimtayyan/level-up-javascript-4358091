e18b928f0328c0d81d31d7b0dc6ee0ef5eb2e613
wasim altayyan <wasimtayyan@gmail.com>
2023-09-03 17:05:32 +0300

    05 done

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,31 @@
 // Write your code here
+// class User {
+//   #username
+//   #password
+//   age
+//   constructor(username, passwoed, age) {
+//     this.#username = username
+//     this.#password = passwoed
+//     this.age = age
+//   }
+
+
+// }
+
+// const wasim = new User('wasim', 32342342, 25)
+// console.log(wasim.age)
+// console.log(wasim.username)
+// console.log(wasim.password)
+
+const username = Symbol('username')
+const password = Symbol('password')
+
+const User = {
+  [username]: 'wasim',
+  [password]: '1343442',
+  age: 25
+}
+
+console.log(User.username)
+console.log(User.password)
+console.log(User.age)

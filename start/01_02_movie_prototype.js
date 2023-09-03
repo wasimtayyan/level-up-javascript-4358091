3680d4dfc6bd12916f6825d829192bf0683d44bf
wasim altayyan <wasimtayyan@gmail.com>
2023-09-03 16:26:35 +0300

    02 challeng done

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,15 @@
 // Write your code here
+function Movie(title, director, genre, releaseDate, rating) {
+  this.title = title
+  this.director = director
+  this.genre = genre
+  this.releaseDate = releaseDate
+  this.rating = rating
+}
+Movie.prototype.GetOverView = function () {
+  return `${this.title} a ${this.genre} film dircted by ${this.director} was reelased in ${this.releaseDate} it receved a rating of ${this.rating}`
+}
+
+const HangerGame = new Movie('Hanger_Game', 'some one', 'action', 2014, 4.5)
+
+console.log(HangerGame.GetOverView())
